d69f5ae52a85fceee5a16a55b2f0b8b246b40d1d
Antoine PLASKOWSKI <plaskowski.stanislas@gmail.com>
2024-08-21 22:45:27 +0200

    0.0.0

---
+++ data.lua
@@ -0,0 +1,2 @@
+data.raw["ammo"]["artillery-shell"].ammo_type.action.action_delivery.starting_speed =
+    settings.startup["artillery-shell-speed"].value
---
+++ settings.lua
@@ -0,0 +1,9 @@
+data:extend {
+  {
+    type = "int-setting",
+    name = "artillery-shell-speed",
+    setting_type = "startup",
+    default_value = 10,
+    minimum_value = 1,
+  },
+}
